d05dacd9975518a1d787dae77afe44d13491c616
Alexey Voznyuk <me@swizard.info>
2021-09-06 16:53:45 +0300

    impl ready for api client

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use std::{
+    time::{
+        Instant,
+        Duration,
+    },
+};
+
 use serde_derive::{
     Deserialize,
 };
@@ -6,6 +13,12 @@ use reqwest::{
     Client,
     StatusCode,
     RequestBuilder,
+};
+
+use tokio::{
+    time::{
+        sleep,
+    },
 };
 
 use async_trait::{
@@ -47,16 +60,109 @@ pub struct Api {
 }
 
 pub struct Solved {
+    answer: String,
+}
+
+impl Solved {
+    pub fn answer(&self) -> &str {
+        &self.answer
+    }
 }
 
 #[derive(Debug)]
 pub enum ApiError<E> {
-    PrepareRequest(E),
-    SendRequest(reqwest::Error),
-    BadStatusCode { status_code: StatusCode, },
-    ReadResponse(reqwest::Error),
-    DecodeResponse(serde_json::Error),
-    UnexpectedApiRequestResponse(ApiRequestResponse),
+    PrepareCaptchaRequest(E),
+    SendCaptchaRequest(reqwest::Error),
+    SendCaptchaRequestBadStatusCode { status_code: StatusCode, },
+    ReadCaptchaResponse(reqwest::Error),
+    DecodeCaptchaResponse(serde_json::Error),
+    CaptchaResponse(CaptchaResponseError),
+    PollResponse(PollResponseError),
+    SendPollRequest(reqwest::Error),
+    SendPollRequestBadStatusCode { status_code: StatusCode, },
+    ReadPollResponse(reqwest::Error),
+    DecodePollResponse(serde_json::Error),
+}
+
+impl Api {
+    pub fn new(api_token: ApiToken, params: Params) -> Api {
+        Api { api_token, params, }
+    }
+
+    pub async fn solve<C>(&self, captcha: C) -> Result<Solved, ApiError<C::PrepareRequestError>> where C: CaptchaRequest {
+        let client = Client::new();
+        let request_builder = client.post(&self.params.api_request_url);
+        let request_builder = captcha.prepare_request(&self.api_token, request_builder).await
+            .map_err(ApiError::PrepareCaptchaRequest)?;
+        let response = request_builder.send().await
+            .map_err(ApiError::SendCaptchaRequest)?;
+        let status_code = response.status();
+        if status_code != StatusCode::OK {
+            return Err(ApiError::SendCaptchaRequestBadStatusCode { status_code, });
+        }
+        let api_response_string = response.text().await
+            .map_err(ApiError::ReadCaptchaResponse)?;
+        let api_response: ApiResponse = serde_json::from_str(&api_response_string)
+            .map_err(ApiError::DecodeCaptchaResponse)?;
+
+        let captcha_id = api_response.extract_captcha_id()
+            .map_err(ApiError::CaptchaResponse)?;
+
+        let get_parameters = [
+            ("key", &*self.api_token.key),
+            ("action", "get"),
+            ("id", &captcha_id),
+            ("json", "1"),
+        ];
+
+        loop {
+            let now = Instant::now();
+
+            let response = client.get(&self.params.api_result_url)
+                .form(&get_parameters)
+                .send()
+                .await
+                .map_err(ApiError::SendPollRequest)?;
+            let status_code = response.status();
+            if status_code != StatusCode::OK {
+                return Err(ApiError::SendPollRequestBadStatusCode { status_code, });
+            }
+            let api_response_string = response.text().await
+                .map_err(ApiError::ReadPollResponse)?;
+            let api_response: ApiResponse = serde_json::from_str(&api_response_string)
+                .map_err(ApiError::DecodePollResponse)?;
+
+            let poll_result = api_response.extract_poll_result()
+                .map_err(ApiError::PollResponse)?;
+            match poll_result {
+                PollResult::NotReady => {
+                    let elapsed = now.elapsed().as_millis() as u64;
+                    if elapsed < self.params.poll_timeout_ms {
+                        sleep(Duration::from_millis(self.params.poll_timeout_ms - elapsed)).await;
+                    }
+                },
+                PollResult::Ready { solved_captcha, } =>
+                    return Ok(Solved { answer: solved_captcha, }),
+            }
+        }
+    }
+}
+
+#[async_trait]
+pub trait CaptchaRequest {
+    type PrepareRequestError;
+
+    async fn prepare_request(&self, api_token: &ApiToken, request_builder: RequestBuilder) -> Result<RequestBuilder, Self::PrepareRequestError>;
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ApiResponse {
+    pub status: i32,
+    pub request: String,
+}
+
+#[derive(Debug)]
+pub enum CaptchaResponseError {
     WrongUserKey,
     KeyDoesNotExist,
     ZeroBalance,
@@ -77,89 +183,116 @@ pub enum ApiError<E> {
     MaxUserTurn,
     BadParameters,
     BadProxy,
+    UnexpectedApiResponse(ApiResponse),
 }
 
-impl Api {
-    pub fn new(api_token: ApiToken, params: Params) -> Api {
-        Api { api_token, params, }
-    }
+#[derive(Debug)]
+pub enum PollResponseError {
+    ErrorCaptchaUnsolvable,
+    ErrorWrongUserKey,
+    ErrorKeyDoesNotExist,
+    ErrorWrongIdFormat,
+    ErrorWrongCaptchaId,
+    ErrorBadDuplicates,
+    ErrorReportNotRecorded,
+    ErrorDuplicateReport,
+    RequestLimitExceeded { code: String, },
+    ErrorIpAddres,
+    ErrorTokenExpired,
+    ErrorEmptyAction,
+    ErrorProxyConnectionFailed,
+    UnexpectedApiResponse(ApiResponse),
+}
 
-    pub async fn solve<C>(&self, captcha: C) -> Result<Solved, ApiError<C::PrepareRequestError>> where C: CaptchaRequest {
-        let client = Client::new();
-        let request_builder = client.post(&self.params.api_request_url);
-        let request_builder = captcha.prepare_request(&self.api_token, request_builder).await
-            .map_err(ApiError::PrepareRequest)?;
-        let response = request_builder.send().await
-            .map_err(ApiError::SendRequest)?;
-        let status_code = response.status();
-        if status_code != StatusCode::OK {
-            return Err(ApiError::BadStatusCode { status_code, });
-        }
-        let api_response_string = response.text().await
-            .map_err(ApiError::ReadResponse)?;
-        let api_response: ApiRequestResponse = serde_json::from_str(&api_response_string)
-            .map_err(ApiError::DecodeResponse)?;
+enum PollResult {
+    NotReady,
+    Ready { solved_captcha: String, },
+}
 
-        let captcha_id = match api_response {
-            ApiRequestResponse { status: 1, request, } =>
-                request,
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_WRONG_USER_KEY" =>
-                return Err(ApiError::WrongUserKey),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_KEY_DOES_NOT_EXIST" =>
-                return Err(ApiError::KeyDoesNotExist),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_ZERO_BALANCE" =>
-                return Err(ApiError::ZeroBalance),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_PAGEURL" =>
-                return Err(ApiError::Pageurl),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_NO_SLOT_AVAILABLE" =>
-                return Err(ApiError::NoSlotAvailable),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_ZERO_CAPTCHA_FILESIZE" =>
-                return Err(ApiError::ZeroCaptchaFilesize),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_TOO_BIG_CAPTCHA_FILESIZE" =>
-                return Err(ApiError::TooBigCaptchaFilesize),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_WRONG_FILE_EXTENSION" =>
-                return Err(ApiError::WrongFileExtension),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_IMAGE_TYPE_NOT_SUPPORTED" =>
-                return Err(ApiError::ImageTypeNotSupported),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_UPLOAD" =>
-                return Err(ApiError::Upload),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_IP_NOT_ALLOWED" =>
-                return Err(ApiError::IpNotAllowed),
-            ApiRequestResponse { status: 0, request, } if request == "IP_BANNED" =>
-                return Err(ApiError::IpBanned),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_BAD_TOKEN_OR_PAGEURL" =>
-                return Err(ApiError::BadTokenOrPageurl),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_GOOGLEKEY" =>
-                return Err(ApiError::Googlekey),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_WRONG_GOOGLEKEY" =>
-                return Err(ApiError::WrongGooglekey),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_CAPTCHAIMAGE_BLOCKED" =>
-                return Err(ApiError::CaptchaimageBlocked),
-            ApiRequestResponse { status: 0, request, } if request == "TOO_MANY_BAD_IMAGES" =>
-                return Err(ApiError::TooManyBadImages),
-            ApiRequestResponse { status: 0, request, } if request == "MAX_USER_TURN" =>
-                return Err(ApiError::MaxUserTurn),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_BAD_PARAMETERS" =>
-                return Err(ApiError::BadParameters),
-            ApiRequestResponse { status: 0, request, } if request == "ERROR_BAD_PROXY" =>
-                return Err(ApiError::BadProxy),
+impl ApiResponse {
+    fn extract_captcha_id(self) -> Result<String, CaptchaResponseError> {
+        match self {
+            ApiResponse { status: 1, request, } =>
+                Ok(request),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_USER_KEY" =>
+                Err(CaptchaResponseError::WrongUserKey),
+            ApiResponse { status: 0, request, } if request == "ERROR_KEY_DOES_NOT_EXIST" =>
+                Err(CaptchaResponseError::KeyDoesNotExist),
+            ApiResponse { status: 0, request, } if request == "ERROR_ZERO_BALANCE" =>
+                Err(CaptchaResponseError::ZeroBalance),
+            ApiResponse { status: 0, request, } if request == "ERROR_PAGEURL" =>
+                Err(CaptchaResponseError::Pageurl),
+            ApiResponse { status: 0, request, } if request == "ERROR_NO_SLOT_AVAILABLE" =>
+                Err(CaptchaResponseError::NoSlotAvailable),
+            ApiResponse { status: 0, request, } if request == "ERROR_ZERO_CAPTCHA_FILESIZE" =>
+                Err(CaptchaResponseError::ZeroCaptchaFilesize),
+            ApiResponse { status: 0, request, } if request == "ERROR_TOO_BIG_CAPTCHA_FILESIZE" =>
+                Err(CaptchaResponseError::TooBigCaptchaFilesize),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_FILE_EXTENSION" =>
+                Err(CaptchaResponseError::WrongFileExtension),
+            ApiResponse { status: 0, request, } if request == "ERROR_IMAGE_TYPE_NOT_SUPPORTED" =>
+                Err(CaptchaResponseError::ImageTypeNotSupported),
+            ApiResponse { status: 0, request, } if request == "ERROR_UPLOAD" =>
+                Err(CaptchaResponseError::Upload),
+            ApiResponse { status: 0, request, } if request == "ERROR_IP_NOT_ALLOWED" =>
+                Err(CaptchaResponseError::IpNotAllowed),
+            ApiResponse { status: 0, request, } if request == "IP_BANNED" =>
+                Err(CaptchaResponseError::IpBanned),
+            ApiResponse { status: 0, request, } if request == "ERROR_BAD_TOKEN_OR_PAGEURL" =>
+                Err(CaptchaResponseError::BadTokenOrPageurl),
+            ApiResponse { status: 0, request, } if request == "ERROR_GOOGLEKEY" =>
+                Err(CaptchaResponseError::Googlekey),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_GOOGLEKEY" =>
+                Err(CaptchaResponseError::WrongGooglekey),
+            ApiResponse { status: 0, request, } if request == "ERROR_CAPTCHAIMAGE_BLOCKED" =>
+                Err(CaptchaResponseError::CaptchaimageBlocked),
+            ApiResponse { status: 0, request, } if request == "TOO_MANY_BAD_IMAGES" =>
+                Err(CaptchaResponseError::TooManyBadImages),
+            ApiResponse { status: 0, request, } if request == "MAX_USER_TURN" =>
+                Err(CaptchaResponseError::MaxUserTurn),
+            ApiResponse { status: 0, request, } if request == "ERROR_BAD_PARAMETERS" =>
+                Err(CaptchaResponseError::BadParameters),
+            ApiResponse { status: 0, request, } if request == "ERROR_BAD_PROXY" =>
+                Err(CaptchaResponseError::BadProxy),
             other =>
-                return Err(ApiError::UnexpectedApiRequestResponse(other)),
-        };
-
-        todo!()
+                Err(CaptchaResponseError::UnexpectedApiResponse(other)),
+        }
     }
-}
 
-#[async_trait]
-pub trait CaptchaRequest {
-    type PrepareRequestError;
-
-    async fn prepare_request(&self, api_token: &ApiToken, request_builder: RequestBuilder) -> Result<RequestBuilder, Self::PrepareRequestError>;
-}
-
-#[derive(Deserialize, Debug)]
-pub struct ApiRequestResponse {
-    pub status: i32,
-    pub request: String,
+    fn extract_poll_result(self) -> Result<PollResult, PollResponseError> {
+        match self {
+            ApiResponse { status: 1, request, } =>
+                Ok(PollResult::Ready { solved_captcha: request, }),
+            ApiResponse { status: 0, request, } if request == "CAPCHA_NOT_READY" =>
+                Ok(PollResult::NotReady),
+            ApiResponse { status: 0, request, } if request == "ERROR_CAPTCHA_UNSOLVABLE" =>
+                Err(PollResponseError::ErrorCaptchaUnsolvable),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_USER_KEY" =>
+                Err(PollResponseError::ErrorWrongUserKey),
+            ApiResponse { status: 0, request, } if request == "ERROR_KEY_DOES_NOT_EXIST" =>
+                Err(PollResponseError::ErrorKeyDoesNotExist),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_ID_FORMAT" =>
+                Err(PollResponseError::ErrorWrongIdFormat),
+            ApiResponse { status: 0, request, } if request == "ERROR_WRONG_CAPTCHA_ID" =>
+                Err(PollResponseError::ErrorWrongCaptchaId),
+            ApiResponse { status: 0, request, } if request == "ERROR_BAD_DUPLICATES" =>
+                Err(PollResponseError::ErrorBadDuplicates),
+            ApiResponse { status: 0, request, } if request == "ERROR_REPORT_NOT_RECORDED" =>
+                Err(PollResponseError::ErrorReportNotRecorded),
+            ApiResponse { status: 0, request, } if request == "ERROR_DUPLICATE_REPORT" =>
+                Err(PollResponseError::ErrorDuplicateReport),
+            ApiResponse { status: 0, request, } if request.starts_with("ERROR:") =>
+                Err(PollResponseError::RequestLimitExceeded { code: request[6 ..].trim().to_string(), }),
+            ApiResponse { status: 0, request, } if request == "ERROR_IP_ADDRES" =>
+                Err(PollResponseError::ErrorIpAddres),
+            ApiResponse { status: 0, request, } if request == "ERROR_TOKEN_EXPIRED" =>
+                Err(PollResponseError::ErrorTokenExpired),
+            ApiResponse { status: 0, request, } if request == "ERROR_EMPTY_ACTION" =>
+                Err(PollResponseError::ErrorEmptyAction),
+            ApiResponse { status: 0, request, } if request == "ERROR_PROXY_CONNECTION_FAILED" =>
+                Err(PollResponseError::ErrorProxyConnectionFailed),
+            other =>
+                Err(PollResponseError::UnexpectedApiResponse(other)),
+        }
+    }
 }
